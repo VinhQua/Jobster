1c28efddea480fc83decff3d55181ca372b5dd3b
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-07-28 15:15:09 +0900

    first upload

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+const Landing = () => {
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="jobster logo" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            adipisci sint numquam rerum fugiat quia, laudantium facilis modi
+            suscipit aut.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </main>
+  );
+};
+
+export default Landing;
