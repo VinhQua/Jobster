7a34fd69bec66b21979cd1825046d075cc213320
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-07-29 15:51:02 +0900

    Register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,16 @@
-import React from "react";
-
+import React, { useState } from "react";
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
 const Register = () => {
+  const [values, setValues] = useState(initialState);
+  const handleChange = (e) => {
+    console.log(e);
+  };
+
   return <div>regise</div>;
 };
 
