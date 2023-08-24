48b03de92c5d2e4740aa1f3b9fda4ae8fbe2c516
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-08-24 08:46:41 +0700

    Stats Item

--- src/components/StatsItem.jsx
+++ src/components/StatsItem.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
-const StatsItem = () => {
-  return <div>StatsItem</div>;
+import Wrapper from "../assets/wrappers/StatItem";
+const StatsItem = ({ count, title, icon, color, bcg }) => {
+  return (
+    <Wrapper color={color} bcg={bcg}>
+      <header>
+        <span className="count">{count}</span>
+        <span className="icon">{icon}</span>
+      </header>
+      <h5 className="title">{title}</h5>
+    </Wrapper>
+  );
 };
 
 export default StatsItem;
